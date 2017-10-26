131786274caf40c562c0f76cd803905f4f84a0a9
hab278 <hab278@nyu.edu>
2017-10-26 12:29:46 -0400

    Add test for PNX reader

--- spec/citero/inputs/readers/pnx_reader_spec.rb
+++ spec/citero/inputs/readers/pnx_reader_spec.rb
@@ -1,169 +1,146 @@
 require 'spec_helper'
 
-# describe Citero::Inputs::Readers::PnxReader do
-#   let(:pnx_input)   { Citero::Inputs::Readers::PnxReader.new(pnx_data) }
-#   describe "#type" do
-#     it "" do
-#       expect(pnx_input.type).to eq "First"
-#     end
-#   end
-#   # describe "#addau" do
-#   #   it "" do
-#   #     expect(pnx_input.addau).to eq "First"
-#   #   end
-#   # end
-#
-#   describe "#publisher" do
-#     it "" do
-#       expect(pnx_input.publisher).to eq "false"
-#     end
-#   end
-#   describe "#pub" do
-#     it "" do
-#       expect(pnx_input.pub).to eq "false"
-#     end
-#   end
-#   describe "#cop" do
-#     it "" do
-#       expect(pnx_input.cop).to eq "false"
-#     end
-#   end
-#   describe "#issn" do
-#     it "" do
-#       expect(pnx_input.issn).to eq "false"
-#     end
-#   end
-#   describe "#eissn" do
-#     it "" do
-#       expect(pnx_input.eissn).to eq "false"
-#     end
-#   end
-#   describe "#isbn" do
-#     it "" do
-#       expect(pnx_input.isbn).to eq "false"
-#     end
-#   end
-#   describe "#title" do
-#     it "" do
-#       expect(pnx_input.title).to eq "false"
-#     end
-#   end
-#   describe "#publication_date" do
-#     it "" do
-#       expect(pnx_input.publication_date).to eq "false"
-#     end
-#   end
-#   describe "#journal_title" do
-#     it "" do
-#       expect(pnx_input.journal_title).to eq "false"
-#     end
-#   end
-#   describe "#date" do
-#     it "" do
-#       expect(pnx_input.date).to eq "false"
-#     end
-#   end
-#   describe "#language" do
-#     it "" do
-#       expect(pnx_input.language).to eq "false"
-#     end
-#   end
-#   describe "#edition" do
-#     it "" do
-#       expect(pnx_input.edition).to eq "false"
-#     end
-#   end
-#   describe "#tags" do
-#     it "" do
-#       expect(pnx_input.tags).to eq "false"
-#     end
-#   end
-#   describe "#call_number" do
-#     it "" do
-#       expect(pnx_input.call_number).to eq "false"
-#     end
-#   end
-#   describe "#pnx_record_id" do
-#     it "" do
-#       expect(pnx_input.pnx_record_id).to eq "false"
-#     end
-#   end
-#   describe "#description" do
-#     it "" do
-#       expect(pnx_input.description).to eq "false"
-#     end
-#   end
-#   describe "#notes" do
-#     it "" do
-#       expect(pnx_input.notes).to eq "false"
-#     end
-#   end
-#   describe "#pages" do
-#     it "" do
-#       expect(pnx_input.pages).to eq "false"
-#     end
-#   end
-#   describe "#identifier" do
-#     it "" do
-#       expect(pnx_input.identifier).to eq "false"
-#     end
-#   end
-#   describe "#creator" do
-#     it "" do
-#       expect(pnx_input.creator).to eq "false"
-#     end
-#   end
-#   describe "#addau" do
-#     it "" do
-#       expect(pnx_input.addau).to eq "false"
-#     end
-#   end
-#   describe "#contributor" do
-#     it "" do
-#       expect(pnx_input.contributor).to eq "false"
-#     end
-#   end
-#     #
-#     # context "with only first name" do
-#     #   let(:name)                { "First" }
-#     #
-#     #   it "returns only the first name" do
-#     #     expect(standardized_name).to eq "First"
-#     #   end
-#     # end
-#     #
-#     # context "with first and last name" do
-#     #   let(:name)                { "First Last" }
-#     #
-#     #   it "returns with 'last name, first name'" do
-#     #     expect(standardized_name).to eq "Last, First"
-#     #   end
-#     # end
-#     #
-#     # context "with first, middle and last name" do
-#     #   let(:name)                { "First Middle Last" }
-#     #
-#     #   it "returns with 'last name, first name middle name'" do
-#     #     expect(standardized_name).to eq "Last, First Middle"
-#     #   end
-#     #
-#     #   context "with superfluous data in parenthesis" do
-#     #     let(:name)                { "First Middle Last (Akks)" }
-#     #
-#     #     it "removes superfluous data in parenthesis, returns with 'last name, first name middle name'" do
-#     #       expect(standardized_name).to eq "Last, First Middle"
-#     #     end
-#     #   end
-#     #   context "with unrelated data pertaining to the person" do
-#     #     let(:name)                { "First  Last (dd) 1943-" }
-#     #
-#     #     it "removes unrelated data pertaining to the person, returns with 'last name, first name middle name'" do
-#     #       expect(standardized_name).to eq "Last, First"
-#     #     end
-#     #   end
-#     # end
-#   # end
-# end
-# stussy
-# yeezy 350
-# gucci
+describe Citero::Inputs::Readers::PnxReader do
+  let(:pnx_input)   { Citero::Inputs::Readers::PnxReader.new(pnx_data) }
+  let(:action)      { pnx_input.send(method_sym) }
+  let(:result)      { "#{method_sym.to_s}-test" }
+  
+  describe "#type" do
+    let(:method_sym)      { :type }
+    it "should give me a value" do
+      expect(action).to eq result
+    end
+  end
+
+  describe "#publisher" do
+    let(:method_sym)      { :publisher }
+    it "should give me a value" do
+      expect(action).to eq result
+    end
+  end
+
+  describe "#language" do
+    let(:method_sym)      { :language }
+    it "should give me a value" do
+      expect(action).to eq result
+    end
+  end
+
+  describe "#edition" do
+    let(:method_sym)      { :edition }
+    it "should give me a value" do
+      expect(action).to eq result
+    end
+  end
+
+  describe "#pages" do
+    let(:method_sym)      { :pages }
+    let(:result)          { "format-test" }
+    it "should give me a value" do
+      expect(action).to eq result
+    end
+  end
+
+  describe "#identifier" do
+    let(:method_sym)      { :identifier }
+    it "should give me a value" do
+      expect(action).to eq result
+    end
+  end
+
+  describe "#creator" do
+    let(:method_sym)      { :creator }
+    it "should give me a value" do
+      expect(action).to eq result
+    end
+  end
+
+  describe "#addau" do
+    let(:method_sym)      { :addau }
+    it "should give me a value" do
+      expect(action).to eq result
+    end
+  end
+
+  describe "#pub" do
+    let(:method_sym)    { :pub }
+    it "should give me a value" do
+      expect(action).to eq [result]
+    end
+  end
+
+  describe "#cop" do
+    let(:method_sym)    { :cop }
+    it "should give me a value" do
+      expect(action).to eq [result]
+    end
+  end
+
+  describe "#issn" do
+    let(:method_sym)    { :issn }
+    it "should give me a value" do
+      expect(action).to eq [result]
+    end
+  end
+
+  describe "#eissn" do
+    let(:method_sym)    { :eissn }
+    it "should give me a value" do
+      expect(action).to eq [result]
+    end
+  end
+
+  describe "#isbn" do
+    let(:method_sym)    { :isbn }
+    it "should give me a value" do
+      expect(action).to eq [result]
+    end
+  end
+
+  describe "#title" do
+    let(:method_sym)    { :title }
+    it "should give me a value" do
+      expect(action).to eq [result]
+    end
+  end
+
+  describe "#journal_title" do
+    let(:method_sym)    { :journal_title }
+    let(:result)        { "jtitle-test" }
+    it "should give me a value" do
+      expect(action).to eq [result]
+    end
+  end
+
+  describe "#description" do
+    let(:method_sym)    { :notes }
+    let(:result)        { "description-test" }
+    it "should give me a value" do
+      expect(action).to eq [result]
+    end
+  end
+
+  describe "#tags" do
+    let(:method_sym)    { :tags }
+    let(:result)        { ["subject-test", "subject-test"] }
+    it "should give me a value" do
+      expect(action).to eq result
+    end
+  end
+
+  describe "#date" do
+    let(:method_sym)    { :date }
+    let(:result)        { ["creationdate-test", "creationdate-test"] }
+    it "should give me a value" do
+      expect(action).to eq result
+    end
+  end
+
+  describe "#publication_date" do
+    let(:method_sym)    { :publication_date }
+    let(:result)        { ["date-test", "date-test"] }
+    it "should give me a value" do
+      expect(action).to eq result
+    end
+  end
+end
